29dea9f396ad692003f7f27038a6918c98aac23e
Joaquin Gimenez <jg@joaquingimenez.com>
2021-09-24 16:56:26 -0400

    dynamic api exposure

--- backend-web/src/index.js
+++ backend-web/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.API_PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
