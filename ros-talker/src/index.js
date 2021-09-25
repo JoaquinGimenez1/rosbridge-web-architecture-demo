7481577d4d008f9775757c5d516cfecafd13dd59
Joaquin Gimenez <jg@joaquingimenez.com>
2021-09-25 11:56:46 -0400

    cleanup docker-compose

--- ros-talker/src/index.js
+++ ros-talker/src/index.js
@@ -1,24 +1,39 @@
 #!/usr/bin/env node
-
 const rosnodejs = require("rosnodejs");
-const std_msgs = rosnodejs.require("std_msgs").msg;
+
+const ROS_HOST = process.env.ROS_HOST || "localhost";
+const ROS_PORT = process.env.ROS_PORT || "11311";
 
 const talker = () => {
-  rosnodejs.initNode("/talker_node").then((rosNode) => {
-    let pub = rosNode.advertise("/chatter", std_msgs.String);
-    let count = 0;
-    const msg = new std_msgs.String();
+  rosnodejs
+    .initNode("/talker_node", {
+      rosMasterUri: `http://${ROS_HOST}:${ROS_PORT}`,
+      // onTheFly allows to generate message without catkin?
+      onTheFly: true,
+    })
+    .then((rosNode) => {
+      // rosnodejs.loadAllPackages();
+      // Need to require message AFTER the node has been initialized
+      // Ref: https://github.com/RethinkRobotics-opensource/rosnodejs#generating-messages
+      const stdMsgs = rosnodejs.require("std_msgs");
 
-    setInterval(() => {
-      // Construct the message
-      msg.data = "hello world " + count;
-      // Publish over ROS
-      pub.publish(msg);
-      // Log through stdout and /rosout
-      rosnodejs.log.info("I said: [" + msg.data + "]");
-      ++count;
-    }, 1000);
-  });
+      let pub = rosNode.advertise("/chatter", stdMsgs.String);
+      // let count = 0;
+      const msg = new stdMsgs.String();
+
+      setInterval(() => {
+        // Construct the message
+        msg.data = `Today is ${new Date().toISOString()}`;
+        // Publish over ROS
+        pub.publish(msg);
+        // Log through stdout and /rosout
+        rosnodejs.log.info("I said: [" + msg.data + "]");
+        // ++count;
+      }, 2000);
+    })
+    .catch((error) => {
+      console.log("Some error occured!", error.message);
+    });
 };
 
 /* istanbul ignore next */
