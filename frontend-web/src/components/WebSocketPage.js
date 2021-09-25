b08bbe65d50a3174f80bffa28a175a737342bcb3
Joaquin Gimenez <jg@joaquingimenez.com>
2021-09-25 18:17:07 -0400

    making it prettier

--- frontend-web/src/components/WebSocketPage.js
+++ frontend-web/src/components/WebSocketPage.js
@@ -1,9 +1,12 @@
 import React, { useState, useCallback, useMemo, useRef } from "react";
+import { Button, Typography, Stack } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+
 import useWebSocket, { ReadyState } from "react-use-websocket";
 
 const API_HOST = process.env.API_HOST || "localhost";
 const API_PORT = process.env.API_PORT || "5000";
-
 const WEB_SOCKET_URL = `ws://${API_HOST}:${API_PORT}`;
 
 const WebSocketPage = () => {
@@ -11,7 +14,10 @@ const WebSocketPage = () => {
   const [socketUrl, setSocketUrl] = useState(WEB_SOCKET_URL);
   const messageHistory = useRef([]);
 
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
+    onOpen: () => console.log("Connection Opened"),
+    shouldReconnect: (closeEvent) => true,
+  });
 
   messageHistory.current = useMemo(
     () => messageHistory.current.concat(lastMessage),
@@ -35,22 +41,37 @@ const WebSocketPage = () => {
 
   return (
     <div>
-      <button onClick={handleClickChangeSocketUrl}>
-        Click Me to change Socket Url
-      </button>
-      <button
-        onClick={handleClickSendMessage}
-        disabled={readyState !== ReadyState.OPEN}
-      >
-        Click me to say hello
-      </button>
-      <span>The WebSocket is currently {connectionStatus}</span>
-      {lastMessage ? <span>Last message: {lastMessage?.data}</span> : null}
-      <ul>
-        {messageHistory.current.map((message, idx) => (
-          <span key={idx}>{message?.data}</span>
-        ))}
-      </ul>
+      <Typography variant="h4">Frontend of the project</Typography>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" onClick={handleClickChangeSocketUrl}>
+          Click Me to change Socket Url
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleClickSendMessage}
+          disabled={readyState !== ReadyState.OPEN}
+        >
+          Click me to send Hello!
+        </Button>
+      </Stack>
+      <br />
+      <Typography>The WebSocket is currently {connectionStatus}</Typography>
+      <Typography>
+        Status: {connectionStatus === "Open" ? <CheckIcon /> : <CloseIcon />}
+      </Typography>
+      <Typography>Last message: {lastMessage?.data}</Typography>
+      <Typography>Passed messages:</Typography>
+
+      <code></code>
+
+      {messageHistory.current.length && (
+        <ul>
+          {messageHistory.current.map((message, idx) => (
+            <li key={idx}>{message?.data}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
