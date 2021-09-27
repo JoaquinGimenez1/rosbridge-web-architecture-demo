6488435cd2cdb36292136aea1ffe54b51aafbcab
Joaquin Gimenez <jg@joaquingimenez.com>
2021-09-27 18:26:39 -0400

    Release v0.3.2 (#14)
    * Bugfix #11 double message print (#13)
    
    * Fix double print in the ui
    
    * linter fixes
    
    * update package version

--- frontend-web/src/components/WebSocketPage.js
+++ frontend-web/src/components/WebSocketPage.js
@@ -1,33 +1,47 @@
-import React, { useState, useCallback, useMemo, useRef } from "react";
-import { Button, Typography, Stack } from "@mui/material";
+import React, { useState, useCallback, useEffect } from "react";
+import { Button, Typography, Divider } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
 
 import useWebSocket, { ReadyState } from "react-use-websocket";
+const useStyles = makeStyles({
+  button: {
+    margin: "10px",
+  },
+  mainInfo: {
+    display: "flex",
+    flexDirection: "column",
+    alignContent: "flex-end",
+    // justifyContent: "left",
+    margin: "10px",
+  },
+  results: {
+    display: "flex",
+    flexDirection: "column",
+  },
+});
 
 const API_HOST = process.env.REACT_APP_API_HOST || "localhost";
 const API_PORT = process.env.REACT_APP_API_PORT || "5000";
 const WEB_SOCKET_URL = `ws://${API_HOST}:${API_PORT}/api`;
 
 const WebSocketPage = () => {
-  //Public API that will echo messages sent to it back to the client
-  const [socketUrl, setSocketUrl] = useState(WEB_SOCKET_URL);
-  const messageHistory = useRef([]);
+  const classes = useStyles();
+  const [messageHistory, setMessageHistory] = useState([]);
 
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
-    onOpen: () => console.log("Connection Opened"),
-    shouldReconnect: (closeEvent) => true,
-  });
-
-  messageHistory.current = useMemo(
-    () => messageHistory.current.concat(lastMessage),
-    [lastMessage]
+  const { sendMessage, lastMessage, readyState } = useWebSocket(
+    WEB_SOCKET_URL,
+    {
+      onOpen: () => console.log("Connection to Backend API opened"),
+      shouldReconnect: (closeEvent) => true,
+    }
   );
 
-  const handleClickChangeSocketUrl = useCallback(
-    () => setSocketUrl(WEB_SOCKET_URL),
-    []
-  );
+  useEffect(() => {
+    setMessageHistory((prevArray) => [lastMessage, ...prevArray]);
+  }, [lastMessage]);
 
   const handleClickSendMessage = useCallback(() => sendMessage("Hello"), []);
 
@@ -43,34 +57,45 @@ const WebSocketPage = () => {
     <div>
       <Typography variant="h4">Frontend of the project</Typography>
 
-      <Stack spacing={2} direction="row">
-        <Button variant="contained" onClick={handleClickChangeSocketUrl}>
-          Click Me to change Socket Url
-        </Button>
-        <Button
-          variant="contained"
-          onClick={handleClickSendMessage}
-          disabled={readyState !== ReadyState.OPEN}
-        >
-          Click me to send Hello!
-        </Button>
-      </Stack>
-      <br />
-      <Typography>The WebSocket is currently {connectionStatus}</Typography>
-      <Typography>
-        Status: {connectionStatus === "Open" ? <CheckIcon /> : <CloseIcon />}
-      </Typography>
-      <Typography>Last message: {lastMessage?.data}</Typography>
-      <Typography>Passed messages:</Typography>
+      <Divider />
 
-      <code></code>
+      <Button
+        className={classes.button}
+        variant="contained"
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Click me to send Hello!
+      </Button>
 
-      {messageHistory.current.length && (
-        <ul>
-          {messageHistory.current.map((message, idx) => (
-            <li key={idx}>{message?.data}</li>
+      <Divider />
+
+      <div className={classes.mainInfo}>
+        <Typography>
+          Status: {connectionStatus === "Open" ? <CheckIcon /> : <CloseIcon />}
+        </Typography>
+        <Typography>The WebSocket is currently {connectionStatus}</Typography>
+
+        <Divider />
+
+        <Typography>Last message: {lastMessage?.data}</Typography>
+
+        <Divider />
+
+        <Typography>Messages list:</Typography>
+      </div>
+
+      {messageHistory.length && (
+        <>
+          {messageHistory.map((message, idx) => (
+            <div key={idx}>
+              <code>
+                Message #{messageHistory.length - idx}, Data: {message?.data}
+              </code>
+              <br />
+            </div>
           ))}
-        </ul>
+        </>
       )}
     </div>
   );
