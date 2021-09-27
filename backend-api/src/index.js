6c878c907d1ae5c350b976f72b4ff5dec80a61ec
Joaquin Gimenez <jg@joaquingimenez.com>
2021-09-27 14:18:47 -0400

    some minor changes

--- backend-api/src/index.js
+++ backend-api/src/index.js
@@ -7,16 +7,27 @@ const expressWs = require("express-ws")(app);
 const HOST = process.env.API_HOST || "localhost";
 const port = process.env.API_PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Please connect via WS");
+app.use((req, res, next) => {
+  // Adding middleware
+  req.newHeader = "New header added by middleware";
+  next();
 });
 
-app.ws("/", (ws, req) => {
-  console.log("Client connected");
+app.get("/", (req, res) => {
+  res.send("Please connect via WS to /api");
+});
+
+app.ws("/api", (ws, req) => {
+  console.log("Client connected to backend API");
 
   const rh = new RosHandler();
-  rh.subscribeToChatter((message) => {
-    ws.send(`I just heard: ${message}`);
+  rh.subscribeToChatter((rosMessage) => {
+    const message = JSON.stringify({
+      extra_field: "Added by Express",
+      payload: rosMessage,
+    });
+
+    ws.send(message);
   });
 
   ws.on("close", () => {
--- frontend-web/src/components/WebSocketPage.js
+++ frontend-web/src/components/WebSocketPage.js
@@ -7,7 +7,7 @@ import useWebSocket, { ReadyState } from "react-use-websocket";
 
 const API_HOST = process.env.REACT_APP_API_HOST || "localhost";
 const API_PORT = process.env.REACT_APP_API_PORT || "5000";
-const WEB_SOCKET_URL = `ws://${API_HOST}:${API_PORT}`;
+const WEB_SOCKET_URL = `ws://${API_HOST}:${API_PORT}/api`;
 
 const WebSocketPage = () => {
   //Public API that will echo messages sent to it back to the client
