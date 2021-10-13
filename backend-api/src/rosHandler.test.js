3766b6b088fb5cb234351e2dd252a27af09c98b8
Joaquin Gimenez <jg@joaquingimenez.com>
2021-10-13 20:37:14 -0300

    partial tests

--- backend-api/src/rosHandler.test.js
+++ backend-api/src/rosHandler.test.js
@@ -1,7 +1,30 @@
 const rosHandler = require("./rosHandler");
 
+let handler;
 describe("rosHandler", () => {
   it("exists", () => {
     expect(rosHandler).toBeDefined();
+  });
+
+  beforeEach(() => {
+    handler = new rosHandler();
+  });
+
+  describe(".connect", () => {
+    it("exists", () => {
+      expect(handler.connect).toBeDefined();
+    });
+  });
+
+  describe(".unsubscribeToChatter", () => {
+    it("exists", () => {
+      expect(handler.unsubscribeToChatter).toBeDefined();
+    });
+  });
+
+  describe(".subscribeToChatter", () => {
+    it("exists", () => {
+      expect(handler.subscribeToChatter).toBeDefined();
+    });
   });
 });
